bf48b84ff15f6816c37a273dbdc2ea4046e89fae
Naman Chanduka <namanchanduka2001@gmail.com>
2021-07-07 21:25:50 +0530

    Initial Setup

---
+++ android/app/src/main/kotlin/com/solodev/calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.solodev.calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Calculator());
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
